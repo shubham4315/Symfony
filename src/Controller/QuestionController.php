b1a85021dfffb941409ec9e089b4689c3033a432
Shubham <shubham4315@gmail.com>
2021-06-22 22:07:32 +0200

    Added some twiggy Goodnness

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class QuestionController{
+class QuestionController extends AbstractController{
     /**
      * @Route("/")
      */
@@ -14,10 +16,20 @@ class QuestionController{
      * @Route("/questions/{slug}")
      */
     public function show($slug){
-        return new Response(sprintf
-            ('Future page to show the question "%s"!',
-                ucwords(str_replace('-',' ',$slug))
-            ));
+
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still',
+            'Honestly, I like furry shoes better than My cat',
+            'Maybe... try saving the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig',[
+            'question'=>ucwords(str_replace('-',' ',$slug)),
+            'answers'=>$answers
+        ]);
+        // return new Response(sprintf
+        //     ('Future page to show the question "%s"!',
+        //         ucwords(str_replace('-',' ',$slug))
+        //     ));
     }
 
 }
